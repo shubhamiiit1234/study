b68b541c01287e69b1a5e8dbde7b0c5121974554
Shubham Kumar <shubhamiiit1234@gmail.com>
2019-12-09 03:08:13 +0530

    factorial

--- new.cpp
+++ new.cpp
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+int factorial(int a)
+{	
+	int n;
+	if(a == 1)
+		return 1;
+	else
+	{
+		n = a*factorial(a-1);
+		//print("%d\n",n);
+		return n;
+	}
+}
 int main()
-{	cout << "hii asus" << endl;
+{	
+	cout << "hii asus" << endl;
 	cout << "good" << endl;
+	cout << factorial(4) << endl;
 	return 0;
 }
